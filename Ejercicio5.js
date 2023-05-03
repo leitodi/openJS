4a3d06337350b66d683c72ebbe568fa7b509fcdc
leitodi <davidleodifra@gmail.com>
2023-05-03 19:30:33 -0300

    Ejercicio 5

---
+++ Ejercicio5.js
@@ -0,0 +1,16 @@
+// Crea un archivo JS que contenga las siguientes líneas
+// - Una variable que contenga tu altura en centímetros (entero)
+// - Una variable que contenga tu altura en metros (número de coma flotante)
+// - Una variable que contenga tu peso en kilogramos (número de coma flotante)
+// - Una variable que contenga tu altura en metros redondeada hacia arriba
+// - Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+// - Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
+
+
+let altura = 158
+let alturaF= 1.58
+let kilos = 78.3
+let kgredondeado = Math.round(kilos);
+let maximo =Number.max_value;
+console.log(kgredondeado);
+console.log(2**1024);
